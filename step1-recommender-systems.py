441547f2dc5ae30e13f1f4812adb1d31c83f15c8
Don Luke Beck <l.becheanu@student.tudelft.nl>
2021-01-20 18:28:44 +0100

    Update step1-recommender-systems.py

--- step1-recommender-systems.py
+++ step1-recommender-systems.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import os.path
 from random import randint
 
 # -*- coding: utf-8 -*-
@@ -16,25 +15,36 @@ To know more about the expectations, please refer to the guidelines.
 ##
 #####
 
-#Where data is located
+# Where data is located
 movies_file = './data/movies.csv'
 users_file = './data/users.csv'
 ratings_file = './data/ratings.csv'
 predictions_file = './data/predictions.csv'
 submission_file = './data/submission.csv'
-
+corr_file = "./data/corr.csv"
 
 # Read the data using pandas
-movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID':'int', 'year':'int', 'movie':'str'}, names=['movieID', 'year', 'movie'])
-users_description = pd.read_csv(users_file, delimiter=';', dtype={'userID':'int', 'gender':'str', 'age':'int', 'profession':'int'}, names=['userID', 'gender', 'age', 'profession'])
-ratings_description = pd.read_csv(ratings_file, delimiter=';', dtype={'userID':'int', 'movieID':'int', 'rating':'int'}, names=['userID', 'movieID', 'rating'])
+movies_description = pd.read_csv(movies_file, delimiter=';', dtype={'movieID': 'int', 'year': 'int', 'movie': 'str'},
+                                 names=['movieID', 'year', 'movie'])
+users_description = pd.read_csv(users_file, delimiter=';',
+                                dtype={'userID': 'int', 'gender': 'str', 'age': 'int', 'profession': 'int'},
+                                names=['userID', 'gender', 'age', 'profession'])
+ratings_description = pd.read_csv(ratings_file, delimiter=';',
+                                  dtype={'userID': 'int', 'movieID': 'int', 'rating': 'int'},
+                                  names=['userID', 'movieID', 'rating'])
 predictions_description = pd.read_csv(predictions_file, delimiter=';', names=['userID', 'movieID'], header=None)
+corr_description = pd.read_csf(corr_file, delimiter=';', header=None)
+
 
 #####
 ##
 ## COLLABORATIVE FILTERING
 ##
 #####
+
+# for user-user, A should be a vector of all movies for some user
+def cosine_similarity(A, B):
+    return np.dot(A, B) / (np.linalg.norm(A) * np.linalg.norm(B))
 
 
 def np_pearson_cor(x, y):
@@ -54,44 +64,26 @@ def np_pearson_cor(x, y):
 
 
 def predict_collaborative_filtering(movies, users, ratings, predictions):
-
-    utilityMatrix = np.empty((users.shape[0]+1, movies.shape[0]+1))
-    utilityMatrix[:] = np.nan
-    # movieMeanVector = np.zeros(movies['movieID'].shape[0]+1)
+    ratingsMatrix = np.zeros(users.shape[0] + 1, movies.shape[0] + 1)
+    userMatrix = np.zeros(users.shape[0] + 1, movies.shape[0] + 1)
 
     for row in ratings[['userID', 'movieID', 'rating']].to_numpy():
-        utilityMatrix[row[0]][row[1]] = row[2]
+        ratingsMatrix[row[0]][row[1]] = row[2]
+        userMatrix[row[0]][row[1]] = row[2]
 
-    """
-    k = 0
-    for row in utilityMatrix:
-        s = 0
-        length = 0
-        for rating in row:
-            s += rating
-            if rating > 0: length+=1
-        if length > 0: movieMeanVector[k]=s/length
-        k+=1
-    """
-    for i in range(0, len(utilityMatrix)):
-        row = utilityMatrix[i]
-        if (np.nansum(row) == 0): continue
+    # generate the rating matrix and user to user matrix once and save them to CSV
+    # So that we can just load it the next time
+    # And don't have to do all calculations again for a huge speedup
+
+    for i in range(0, len(ratingsMatrix)):
+        row = ratingsMatrix[i]
+        if np.nansum(row) == 0:
+            continue
         pearsonCor = []
-        for j in range(i+1, len(utilityMatrix)):
-            row2 = utilityMatrix[j]
-            # pearson = 0
-            """
-            if row2.sum() != 0:
-                xNormalized = row - np.mean(row, axis=0)
-                yNormalized = row2 - np.mean(row2, axis=0)
-                result = np.dot(xNormalized, yNormalized) \
-                         / np.sqrt(np.outer(np.sum(xNormalized**2, axis=0), np.sum(yNormalized**2, axis=0)))
-                if (result[0][0] > 1): pearson = 1
-                elif (result[0][0] < -1): pearson = -1
-                else: pearson = result[0][0]
-            """
-            #pearsonCor.append([pearson, j])
+        for j in range(i + 1, len(ratingsMatrix)):
+            row2 = ratingsMatrix[j]
             pearsonCor.append([np_pearson_cor(row, row2), j])
+        """
         maxx1 = [-1, 0]
         maxx2 = [-1, 0]
         for cor in pearsonCor:
@@ -108,27 +100,30 @@ def predict_collaborative_filtering(movies, users, ratings, predictions):
         for entry in range(0, len(row)):
             if row[entry] == np.nan:
                 weightSum = maxx1[0] + maxx2[0]
-                if weightSum == 0: utilityMatrix[i][entry] = 0
+                if weightSum == 0:
+                    ratingsMatrix[i][entry] = 0
                 else:
-                    weightedAverage = (maxx1[0] * utilityMatrix[maxx1[1]][entry]
-                                       + maxx2[0] * utilityMatrix[maxx2[1]][entry]) / weightSum
-                    utilityMatrix[i][entry] = weightedAverage
+                    weightedAverage = (maxx1[0] * ratingsMatrix[maxx1[1]][entry]
+                                       + maxx2[0] * ratingsMatrix[maxx2[1]][entry]) / weightSum
+                    ratingsMatrix[i][entry] = weightedAverage
+    """
     finalPredictions = []
     i = 1
     for row in predictions[['userID', 'movieID']].to_numpy():
-        if utilityMatrix[row[1]][row[0]] != np.nan:
-            finalPredictions.append([i, utilityMatrix[row[1]][row[0]]])
+        if ratingsMatrix[row[1]][row[0]] != np.nan:
+            finalPredictions.append([i, ratingsMatrix[row[1]][row[0]]])
         else:
             finalPredictions.append([i, 0])
         i += 1
     return finalPredictions
+
 
 #####
 ##
 ## LATENT FACTORS
 ##
 #####
-    
+
 def predict_latent_factors(movies, users, ratings, predictions):
     """
     ## TO COMPLETE
@@ -140,8 +135,8 @@ def predict_latent_factors(movies, users, ratings, predictions):
         ratingsMatrix[row[0]][row[1]] = row[2]
     """
     pass
-    
-    
+
+
 #####
 ##
 ## FINAL PREDICTORS
@@ -151,32 +146,34 @@ def predict_latent_factors(movies, users, ratings, predictions):
 def predict_final(movies, users, ratings, predictions):
     return predict_collaborative_filtering(movies, users, ratings, predictions)
 
+
 #####
 ##
 ## RANDOM PREDICTORS
 ##
 #####
-    
-#By default, predicted rate is a random classifier
+
+# By default, predicted rate is a random classifier
 def predict_random(movies, users, ratings, predictions):
     number_predictions = len(predictions)
-    #predict_collaborative_filtering(movies, users, ratings, predictions)
+    # predict_collaborative_filtering(movies, users, ratings, predictions)
     return [[idx, randint(1, 5)] for idx in range(1, number_predictions + 1)]
+
 
 #####
 ##
 ## SAVE RESULTS
 ##
-#####    
+#####
 
 predictions = predict_final(movies_description, users_description, ratings_description, predictions_description)
 
-#Save predictions, should be in the form 'list of tuples' or 'list of lists'
+# Save predictions, should be in the form 'list of tuples' or 'list of lists'
 with open(submission_file, 'w') as submission_writer:
-    #Formates data
+    # Formats data
     predictions = [map(str, row) for row in predictions]
     predictions = [','.join(row) for row in predictions]
-    predictions = 'Id,Rating\n'+'\n'.join(predictions)
-    
-    #Writes it down
+    predictions = 'Id,Rating\n' + '\n'.join(predictions)
+
+    # Writes it down
     submission_writer.write(predictions)
